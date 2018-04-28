f56fbc17274853549ee2e296e4da0c1d54084f05
mgh3326 <mgh3326@naver.com>
2018-04-28 10:03:30 +0900

    update

--- calc.js
+++ calc.js
@@ -3,13 +3,13 @@ var myindex = 0;
 $(document).ready(function () {
     var parser = math.parser();
 
-    if (myindex===0)//0번째 페이지
+    if (myindex === 0)//0번째 페이지
     {
         $("#p0").show();
         $("#s0").hide();
 
     }
-    else if (myindex===1)//1번째 페이지
+    else if (myindex === 1)//1번째 페이지
     {
 
     }
@@ -59,26 +59,23 @@ $(document).ready(function () {
                 else if ($(this).text() === '☞') {
                     myindex++;
                 }
-                else if($(this).text() === 'DEL'){
-                    posi = getCaretPosition(textbox);
-                    if(displayValue.length>0 && posi.end != 0 && displayValue != ''){ // 빈문자열이 아니고 커서의 위치가 맨 앞이 아닐 경우
-                        var str="";
-                        if(posi.start==posi.end){ // 한문자만 삭제하는 경우
-                            str = displayValue.substring(0, posi.start-1);
-                            len = 1;
-                        }
-                        else{ // 드래그 영역을 삭제하는 경우
-                            str = displayValue.substring(0, posi.start);
-                            len = 0;
-                        }
-                        str += displayValue.substring(posi.end, displayValue.length);
-                        displayValue = str;
-                        hisSave(displayValue, resultValue);
-                        $('#display').text(displayValue);
-                        setCaretPosition(textbox, 0, posi.start-len, posi.start-len);
+                else if ($(this).text() === 'DEL') {
+                    if (displayValue.length > 0) { // 빈문자열이 아닐 경우
+                        $('#ohoh').text(displayValue.substring(0, displayValue.length - 1));//이거 맨 뒤 한글자만 줄일때
                     }
                 }
+                else if ($(this).text() === 'ANS') {
+                    displayValue += $('#result').text();
 
+                    $('#ohoh').text(displayValue);
+                }
+                else if ($(this).text() === 'sin' || $(this).text() === 'cos' || $(this).text() === 'tan' ||
+                    $(this).text() === 'exp' || $(this).text() === 'det' || $(this).text() === 'sqrt' ||
+                    $(this).text() === 'log' || $(this).text() === 'inv' || $(this).text() === 'cross') {
+                    displayValue += $(this).text();
+                    displayValue += '(';
+                    $('#ohoh').text(displayValue);
+                }
                 else {
                     displayValue += $(this).text();
                     $('#ohoh').text(displayValue);
