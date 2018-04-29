963068219101f8fafdcf9c3cc713ffbac993fe1c
mgh3326 <mgh3326@naver.com>
2018-04-29 11:38:29 +0900

    update

--- calc.js
+++ calc.js
@@ -9,19 +9,18 @@ $(document).ready(function () {
     $("#lower").show();
     $("#capital").hide();
 
-    if (myindex === 0)//0번째 페이지
+    if (myindex === 0) //0번째 페이지
     {
 
 
-    }
-    else if (myindex === 1)//1번째 페이지
+    } else if (myindex === 1) //1번째 페이지
     {
 
     }
     var displayValue = '0';
-    $('#ohoh').text(displayValue);//result에 결과물을 넣음
-    $('#result').text(displayValue);//result에 결과물을 넣음
-    $('#latex').html('$$' + math.parse(displayValue).toTex() + '$$');//latex에 넣는건가?
+    $('#ohoh').text(displayValue); //result에 결과물을 넣음
+    $('#result').text(displayValue); //result에 결과물을 넣음
+    $('#latex').html('$$' + math.parse(displayValue).toTex() + '$$'); //latex에 넣는건가?
 
     $('.key').each(function (index, key) {
         $(this).click(function (e) {
@@ -49,50 +48,42 @@ $(document).ready(function () {
                     $('#result').text(displayValue);
                     $('#ohoh').text(displayValue);
 
-                }
-                else if ($(this).text() === 'res') {
+                } else if ($(this).text() === 'res') {
                     // $("#p1").hide();
-                }
-                else if ($(this).text() === 'Shift') {
+                } else if ($(this).text() === 'Shift') {
                     $("#p0").toggle();
                     $("#s0").toggle();
                     $("#capital").toggle();
                     $("#lower").toggle();
 
 
-                }
-                else if ($(this).text() === 'Mode') {
+                } else if ($(this).text() === 'Mode') {
                     $("#yeah").toggle();
                     $("#keyboard").toggle();
 
-                }
-                else if ($(this).text() === '☜') {
+                } else if ($(this).text() === '☜') {
                     myindex--;
-                }
-                else if ($(this).text() === '☞') {
+                } else if ($(this).text() === '☞') {
                     myindex++;
-                }
-                else if ($(this).text() === 'DEL') {
+                } else if ($(this).text() === 'DEL') {
                     if (displayValue.length > 0) { // 빈문자열이 아닐 경우
-                        str="";
-                        str= displayValue.substring(0, displayValue.length - 1);
-                        displayValue=str;
-                        $('#ohoh').text(displayValue);//이거 맨 뒤 한글자만 줄일때
+                        str = "";
+                        str = displayValue.substring(0, displayValue.length - 1);
+                        displayValue = str;
+                        $('#ohoh').text(displayValue); //이거 맨 뒤 한글자만 줄일때
                     }
-                }
-                else if ($(this).text() === 'ANS') {
+                } else if ($(this).text() === 'ANS') {
                     displayValue += $('#result').text();
 
                     $('#ohoh').text(displayValue);
-                }
-                else if ($(this).text() === 'sin' || $(this).text() === 'cos' || $(this).text() === 'tan' ||
+                } else if ($(this).text() === 'sin' || $(this).text() === 'cos' || $(this).text() === 'tan' ||
+                    $(this).text() === 'asin' || $(this).text() === 'acos' || $(this).text() === 'atan' ||
                     $(this).text() === 'exp' || $(this).text() === 'det' || $(this).text() === 'sqrt' ||
                     $(this).text() === 'log' || $(this).text() === 'inv' || $(this).text() === 'cross') {
                     displayValue += $(this).text();
                     displayValue += '(';
                     $('#ohoh').text(displayValue);
-                }
-                else {
+                } else {
                     displayValue += $(this).text();
                     $('#ohoh').text(displayValue);
 
